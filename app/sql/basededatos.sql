3448a68831a6614854fb5a0bf76774c7189a48b9
daberoz <daberoz98@gmail.com>
2020-05-25 16:12:40 +0200

    feat(#2)SQL

---
+++ app/sql/basededatos.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.8-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para despliegue
+CREATE DATABASE IF NOT EXISTS `despliegue` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `despliegue`;
+
+-- Volcando estructura para tabla despliegue.personas
+CREATE TABLE IF NOT EXISTS `personas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla despliegue.personas: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
+INSERT INTO `personas` (`id`, `Nombre`, `created_at`, `updated_at`) VALUES
+	(1, 'Dani', '2020-05-21 18:56:16', '2020-05-21 18:56:16');
+/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
